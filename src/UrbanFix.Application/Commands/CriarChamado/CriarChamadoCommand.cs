6a32b2e1a872bf914217bd35e8d799d1a1321d3c
João <GabrielAlvees616@gmail.com>
2026-09-25 02:26:08 -0300

    Criado e Implementado Command de Criacao de Chamado

---
+++ src/UrbanFix.Application/Commands/CriarChamado/CriarChamadoCommand.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UrbanFix.Domain.Enums;
+
+namespace UrbanFix.Application.Commands.CriarChamado
+{
+    public record CriarChamadoCommand(TipoDeProblema Tipo,string Descricao,string CEP,string Numero);
+}
